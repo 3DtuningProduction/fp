7900ad92637b36a4fb3b1c740573e6f8ef6f2812
Ашот Киракосян <ashenemy@gmail.com>
2022-06-13 12:46:19 +0400

    initial commit

---
+++ apps/final-price/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'final-price-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'final-price';
+}
